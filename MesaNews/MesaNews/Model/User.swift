11029e87e4f8e6fd2e22b2867fc9aa5e77fed7c0
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-08 20:09:28 -0300

    Add User model

---
+++ MesaNews/MesaNews/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  MesaNews
+//
+//  Created by RodrigoSA on 3/8/21.
+//
+
+import Foundation
+
+
+struct User {
+    var name: String
+    var email: String
+    var password: String
+}
