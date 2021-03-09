8d4958250e54525ea724b5e24bbaad333c718639
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-09 15:36:31 -0300

    Test api request

--- MesaNews/MesaNews/ViewController.swift
+++ MesaNews/MesaNews/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let mesa = MesaAPIService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mesa.signinUserRequest(email: "john@doe.com", password: "123456") {
+            (data: APIAuthDataSet?, error: String?) in
+            print("rdsa - token: \(String(describing: data?.token))")
+        }
     }
 
 
