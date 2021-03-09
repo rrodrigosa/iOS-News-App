a02fe327df38e1b3673c137754fe8e1a445fcc43
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-09 18:51:04 -0300

    Remove unused code and blank lines

--- MesaNews/MesaNews/AuthViewController.swift
+++ MesaNews/MesaNews/AuthViewController.swift
@@ -20,26 +20,17 @@ class AuthViewController: UIViewController, AuthViewProtocol {
     let newsFeedIdentifier = "NewsFeedIdentifier"
     let registerIdentifier = "RegisterIdentifier"
     
-    let mesa = MesaAPIService()
+//    let mesa = MesaAPIService()
     
     var presenter: AuthPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
         presenter = AuthPresenter(view: self)
     }
     
     @IBAction func loginButton(_ sender: Any) {
-//        mesa.signinUserRequest(email: "john@doe.com", password: "123456") {
-//            (data: APIAuthDataSet?, error: String?) in
-//            print("rdsa - token: \(String(describing: data?.token))")
-//            
-//            self.performSegue(withIdentifier: self.newsFeedIdentifier, sender: sender)
-//        }
-        
         guard let email = emailTextField.text, let password = passwordTextField.text else {
             return
         }
