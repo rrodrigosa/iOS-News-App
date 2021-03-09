b6e1bed5a28eab9e0c8ca4386abf49f3a234c750
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-09 20:12:21 -0300

    Add RegisterPresenter and APIRegisterDataSet model

---
+++ MesaNews/MesaNews/Model/APIRegisterDataSet.swift
@@ -0,0 +1,13 @@
+//
+//  APIRegisterDataSet.swift
+//  MesaNews
+//
+//  Created by RodrigoSA on 3/9/21.
+//
+
+import Foundation
+
+struct APIRegisterDataSet: Codable {
+    let token: String?
+//    let error: Error?
+}
---
+++ MesaNews/MesaNews/Presenter/RegisterPresenter.swift
@@ -0,0 +1,42 @@
+//
+//  RegisterPresenter.swift
+//  MesaNews
+//
+//  Created by RodrigoSA on 3/9/21.
+//
+
+import Foundation
+
+protocol RegisterPresenterProtocol {
+    init(view: RegisterViewProtocol)
+    func signup(name: String, email: String, password: String)
+}
+
+class RegisterPresenter: RegisterPresenterProtocol {
+    unowned let view: RegisterViewProtocol
+    private let mesaAPIService = MesaAPIService()
+    private var apiAuthDataSet: APIAuthDataSet?
+    
+    required init(view: RegisterViewProtocol) {
+        self.view = view
+    }
+    
+    func signup(name: String, email: String, password: String) {
+        mesaAPIService.signupUserRequest(name: name, email: email, password: password) {
+            (data: APIRegisterDataSet?, error: String?) in
+            print("rdsa - (presenter) | token: \(String(describing: data?.token))")
+            guard let data = data else {
+                return
+            }
+            
+            if data.token != nil {
+                self.view.createSuccessfulAlert()
+            } else {
+                self.view.createErrorAlert(message: "decode errors")
+            }
+        }
+    }
+    
+    
+    
+}
