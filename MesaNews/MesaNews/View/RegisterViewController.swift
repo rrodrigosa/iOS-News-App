ab6486a778e5ac88df3d348977ed3eed1b60c3ac
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-09 20:16:45 -0300

    Remove unused code and blank lines

--- MesaNews/MesaNews/View/RegisterViewController.swift
+++ MesaNews/MesaNews/View/RegisterViewController.swift
@@ -19,18 +19,9 @@ class RegisterViewController: UIViewController, RegisterViewProtocol {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     func createErrorAlert(message: String) {
         let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
@@ -43,6 +34,5 @@ class RegisterViewController: UIViewController, RegisterViewProtocol {
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-    */
 
 }
