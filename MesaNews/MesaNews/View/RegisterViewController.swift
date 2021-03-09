16f8fe13bfd525ea17cc75b4b53c193bca6fbc9a
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-09 20:15:38 -0300

    Update RegisterViewController with protocol functions

--- MesaNews/MesaNews/View/RegisterViewController.swift
+++ MesaNews/MesaNews/View/RegisterViewController.swift
@@ -7,7 +7,15 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+protocol RegisterViewProtocol: class {
+    func createErrorAlert(message: String)
+    func createSuccessfulAlert()
+}
+
+class RegisterViewController: UIViewController, RegisterViewProtocol {
+    
+    
+    var presenter: RegisterPresenter?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +31,17 @@ class RegisterViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+    func createErrorAlert(message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func createSuccessfulAlert() {
+        let message = "Registration successful" //localize
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     */
 
