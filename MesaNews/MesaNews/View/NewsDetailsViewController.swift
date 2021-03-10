7b5265c743f0e2858b05ff15d3a0ff229981385f
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-10 07:16:16 -0300

    Add NewsDetailsViewController

---
+++ MesaNews/MesaNews/View/NewsDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsDetailsViewController.swift
+//  MesaNews
+//
+//  Created by RodrigoSA on 3/10/21.
+//
+
+import UIKit
+
+class NewsDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
