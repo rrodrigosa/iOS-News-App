0e7c67355d2e3803495fac546fe449a2eacdb4d5
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-10 07:38:51 -0300

    Update NewsDetailsViewController with web kit and implementation of web view

--- MesaNews/MesaNews/View/NewsDetailsViewController.swift
+++ MesaNews/MesaNews/View/NewsDetailsViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import WebKit
 
-class NewsDetailsViewController: UIViewController {
-
+class NewsDetailsViewController: UIViewController, WKUIDelegate {
+    @IBOutlet weak var newsDetailsWebView: WKWebView!
+    var newsUrl: URL?
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        newsDetailsWebView = WKWebView(frame: .zero, configuration: webConfiguration)
+        newsDetailsWebView.uiDelegate = self
+        view = newsDetailsWebView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let myURL = newsUrl
+        let myRequest = URLRequest(url: myURL!)
+        newsDetailsWebView.load(myRequest)
     }
 
 }
