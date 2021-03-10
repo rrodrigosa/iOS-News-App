b9a5e6d76de8482e31865eb0b2ba8f714bbec9a3
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-10 05:43:54 -0300

    Update current page when list gets to the end

--- MesaNews/MesaNews/Presenter/NewsFeedPresenter.swift
+++ MesaNews/MesaNews/Presenter/NewsFeedPresenter.swift
@@ -16,17 +16,19 @@ class NewsFeedPresenter: NewsFeedPresenterProtocol {
     unowned let view: NewsFeedViewProtocol
     private let mesaAPIService = MesaAPIService()
     private var newsList: [APINewsFeedData] = []
+    private var currentPage = 1
     
     required init(view: NewsFeedViewProtocol) {
         self.view = view
     }
     
     func newsFeedRequest(authToken: String) {
-        mesaAPIService.newsFeedRequest(authToken: authToken) {
+        mesaAPIService.newsFeedRequest(authToken: authToken, currentPage: currentPage) {
             (data: [APINewsFeedData]?, error: String?) in
             guard let data = data else {
                 return
             }
+            self.currentPage+=1
             self.newsList.append(contentsOf: data)
             self.view.populateTable(newsList: self.newsList)
         }
