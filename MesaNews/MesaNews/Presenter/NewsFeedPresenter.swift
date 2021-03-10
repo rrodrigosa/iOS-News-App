2a474195487fa5b158b4dfa9fc56e9d374828fdc
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-10 02:25:06 -0300

    Add NewsFeedPresenterProtocol and NewsFeedPresenter implementation

--- MesaNews/MesaNews/Presenter/NewsFeedPresenter.swift
+++ MesaNews/MesaNews/Presenter/NewsFeedPresenter.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol NewsFeedPresenterProtocol {
+    init(view: NewsFeedViewProtocol)
+    func newsFeedRequest(authToken: String)
+}
+
+class NewsFeedPresenter: NewsFeedPresenterProtocol {
+    unowned let view: NewsFeedViewProtocol
+    private let mesaAPIService = MesaAPIService()
+    private var newsList: [APINewsFeedData] = []
+    
+    required init(view: NewsFeedViewProtocol) {
+        self.view = view
+    }
+    
+    func newsFeedRequest(authToken: String) {
+        mesaAPIService.newsFeedRequest(authToken: authToken) {
+            (data: [APINewsFeedData]?, error: String?) in
+            print("rdsa - (NewsFeedViewController) - data from api")
+            guard let data = data else {
+                return
+            }
+            self.newsList.append(contentsOf: data)
+            self.view.populateTable(newsList: self.newsList)
+        }
+    }
+    
+}
