0529343e8c09c14e6a92c01cb307e4975d0d60f8
RodrigoSA <rrodrigo.sa@gmail.com>
2021-03-10 03:20:47 -0300

    Add NewsFeedImageManager

---
+++ MesaNews/MesaNews/Utils/NewsFeedImageManager.swift
@@ -0,0 +1,16 @@
+//
+//  NewsFeedImageManager.swift
+//  MesaNews
+//
+//  Created by RodrigoSA on 3/10/21.
+//
+
+import Foundation
+import AlamofireImage
+
+class NewsFeedImageManager {
+    
+    func configureImage(newsCell: APINewsFeedData, cell: NewsFeedCell, completion: @escaping (UIImage) -> Void) {
+        
+    }
+}
